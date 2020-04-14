fc95002644c2a86351cf92a999c45b6671c42093
Pieter De Vis <pieter.de.vis@npl.co.uk>
2020-04-14 17:38:07 +0100

    commit

--- setup.py
+++ setup.py
@@ -8,22 +8,21 @@ from io import open
 # but avoids importing the module which may not be installed yet:
 here = path.abspath(path.dirname(__file__))
 __version__ = None
-with open('kumara/version.py') as f:
+with open('punpy/version.py') as f:
     exec(f.read())
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='kumara',
+setup(name='punpy',
       version=__version__,
-      description='liKelihood-based Uncertainties from Mcmc Atmospheric Retrieval Algorithm package',
+      description='Propagating UNcertainties in PYthon',
       author='Pieter De vis',
       long_description=long_description,
       author_email='pieter.de.vis@npl.co.uk',
       url='http://www.npl.co.uk',
-      keywords="MCMC atmospheric retrieval",
+      keywords="uncertainty propagation measurement function",
       packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      install_requires=['numpy',
-                        'emcee'],
+      install_requires=['numpy'],
 )
