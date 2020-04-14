68459a359d7968bfeda14bdd594c2f86159a4554
Pieter De Vis <pieter.de.vis@npl.co.uk>
2020-04-14 17:40:44 +0100

    commit

--- kumara/__init__.py
+++
@@ -1,4 +0,0 @@
-from kumara.forwardModel.ForwardModel import ForwardModel
-from kumara.forwardModel.ForwardModelFactory import ForwardModelFactory
-from kumara.retrieval.RetrievalFactory import RetrievalFactory
-from kumara.sensors.SensorFactory import SensorFactory
